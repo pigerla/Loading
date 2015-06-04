14271ae54f7d5416316ea6ab32ca564bff47eed6
ImSpy <wjj474957860@gmail.com>
2015-06-05 00:48:56 +0800

    complete Loading plugin

--- Loading.js
+++ Loading.js
@@ -1,8 +1,9 @@
 /**
+ * A loading plugin of javascript.
  * Created by spy on 15-6-2.
  */
 
-// create a closure to wrap our code
+// Create a closure to wrap our code
 ;(function(){
 
   // Define our constructor
@@ -38,9 +39,7 @@
     initializeEvents.call(this);
 
     this.loading.className = this.loading.className + ' loading-show';
-    if (this.overlay) {
-      this.overlay.className = this.overlay.className + " loading-show";
-    }
+    this.overlay.className = this.overlay.className + ' loading-show';
   };
 
   // Close loading
@@ -50,21 +49,20 @@
 
     // Remove the open class name
     this.loading.className = this.loading.className.replace(' loading-show', '');
-    /*
-     * Remove the nodes from the DOM
-     */
-    _.loading.parentNode.removeChild(_.loading);
-    if (_.overlay) {
-      _.overlay.parentNode.removeChild(_.overlay);
-    }
-    document.head.removeChild(_.styleSheet);
 
+    // Remove the nodes from the DOM
+    _.loading.parentNode.removeChild(_.loading);
+    _.overlay.parentNode.removeChild(_.overlay);
+    if (_.styleSheet) {
+      _.styleSheet.parentNode.removeChild(_.styleSheet);
+    }
   };
 
   // Building a Loading
   function buildOut () {
     var content, contentHolder, docFrag;
 
+    // Create default stylesheet domNode
     var styleString = styleStr();
     this.styleSheet = document.createElement('style');
     this.styleSheet.type = 'text/css';
@@ -105,7 +103,7 @@
     // Create content area and append to loading
     contentHolder = document.createElement('div');
     contentHolder.className = 'Loading-content';
-    contentHolder.innerHTML = this.options.content === '' ? '<div class="circle">' : content;
+    contentHolder.innerHTML = this.options.content === '' ? '<div class="circle"></div>' : content;
     this.loading.appendChild(contentHolder);
 
     // Append loading to DocumentFragment
@@ -127,16 +125,16 @@
     return source;
   }
 
+  // Initialize events
   function initializeEvents () {
     if (this.overlay) {
       this.overlay.addEventListener('click', this.hide.bind(this));
     }
   }
 
+  // Default loading stylesheet string
   function styleStr () {
     return '@-webkit-keyframes spin{100%{-webkit-transform:rotate(360deg);-o-transform:rotate(360deg);-moz-transform:rotate(360deg);transform:rotate(360deg)}}@keyframes spin{100%{-webkit-transform:rotate(360deg);-o-transform:rotate(360deg);-moz-transform:rotate(360deg);transform:rotate(360deg)}}.circle{width:50px;height:50px;border-radius:50%;-webkit-box-sizing:border-box;-o-box-sizing:border-box;box-sizing:border-box;border:5px solid #ddd;border-top-color:#9a3;-webkit-animation:spin 1s infinite linear}.Loading-overlay{position:fixed;z-index:9998;top:0;left:0;display:none;width:100%;height:100%;background:rgba(0,0,0,.4)}.Loading{position:fixed;z-index:9999;top:50%;left:50%;display:none;-webkit-transform:translate(-50%,-50%);-moz-transform:translate(-50%,-50%);-ms-transform:translate(-50%,-50%);-o-transform:translate(-50%,-50%);transform:translate(-50%,-50%);background:0 0}.Loading-overlay.loading-show,.Loading.loading-show{display:block}.opacity0{opacity:0}';
   }
-
-
 }());
 
